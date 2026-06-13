80e9a7d7028d8d119255978ec9cfb818d5f12c27
Divyesh Nalam <116959406+divyeshnalam@users.noreply.github.com>
2026-06-13 09:13:43 +0530

    Update urls.py

--- vaibhava_temple/urls.py
+++ vaibhava_temple/urls.py
@@ -15,20 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
-admin.site.site_header  = "Sri Vaibhava Venkateswara Swamy Portal"
-admin.site.site_title   = "Sri Vaibhava Venkateswara Swamy Administration"
-admin.site.index_title  = "Temple Content Management"
+admin.site.site_header = "Sri Vaibhava Venkateswara Swamy Portal"
+admin.site.site_title = "Sri Vaibhava Venkateswara Swamy Administration"
+admin.site.index_title = "Temple Content Management"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/",   include("temple_content.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+    path("api/", include("temple_content.urls")),
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Explicit media serving
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
